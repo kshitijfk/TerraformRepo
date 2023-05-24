ad9c6cbe1b4a29ae91d1dd80f80a428632af3cd2
Kshitij <kshitij009nayak@gmail.com>
2023-05-24 15:16:31 +0530

    restore ec2 through snapshot

---
+++ AMI/amidetails.tf
@@ -0,0 +1,27 @@
+provider "aws"{
+    profile =  "default"
+    region = "us-west-2"
+}
+
+
+data "aws_ami" "MyAMI" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
