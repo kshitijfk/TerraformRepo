3e3f48780197602b1cc9ec966115bc85a8fda1d5
Kshitij <kshitij009nayak@gmail.com>
2023-05-24 12:55:29 +0530

    restore ec2 through snapshot

--- restore.tf
+++ restore.tf
@@ -1,16 +1,16 @@
 #provider
-provider "aws" {
+provider "aws"{
     profile =  "default"
     region = "us-west-2"
 }
 
 #resource
-resource “aws_ami” “my_new_ami” {
+resource “aws_ami” “my_new_ami”{
     name="myAMI"
     virtualization_type="hvm"
     root_device_name="/dev/xvda"
 
-    ebs_block_devices {
+    ebs_block_devices{
         device_name="/dev/xvda"
         snapshot_id=
         volume_size=8
